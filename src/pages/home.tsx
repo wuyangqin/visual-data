14a6354ee91c3ddba9bd8621f21ee368db749769
yang <shiokoy1209@gmail.com>
2021-04-29 17:40:03 +0800

    构建大致的grid布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,16 @@ import './home.scss';
 import headerBg from '../static/images/header.png'
 
 export const Home = () => {
-  return (
-    <div>
-      <div className="home">
-          <header style={{backgroundImage:`url(${headerBg})`}}></header>
-      </div>
-    </div>
-  );
+    return (
+        <div className="home">
+            <header style={{backgroundImage: `url(${headerBg})`}}></header>
+            <main>
+                <section className="section1"></section>
+                <section className="section2"></section>
+                <section className="section3"></section>
+                <section className="section4"></section>
+                <section className="section5"></section>
+            </main>
+        </div>
+    );
 };
